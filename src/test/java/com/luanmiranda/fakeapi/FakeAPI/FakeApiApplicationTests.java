c54f2d0d36c7223e90612864c4b8296712948dc7
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-20 18:29:20 -0300

    chore: Setup da API com Spring

---
+++ src/test/java/com/luanmiranda/fakeapi/FakeAPI/FakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luanmiranda.fakeapi.FakeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
