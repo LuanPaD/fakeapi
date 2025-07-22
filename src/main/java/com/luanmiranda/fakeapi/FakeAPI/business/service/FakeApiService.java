57241e8ec9b35c10de031dd0da65f20dda224029
Luan <111983442+LuanPaD@users.noreply.github.com>
2025-07-22 20:26:05 -0300

    fix: Correção para salvar produtos no banco

--- src/main/java/com/luanmiranda/fakeapi/FakeAPI/business/service/FakeApiService.java
+++ src/main/java/com/luanmiranda/fakeapi/FakeAPI/business/service/FakeApiService.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+import static java.lang.String.format;
+
 @Service
 @RequiredArgsConstructor
 public class FakeApiService {
@@ -17,12 +19,20 @@ public class FakeApiService {
     private final ProdutoService produtoService;
 
     public List<ProductsDTO> buscarProdutos() {
-        List<ProductsDTO> produtosDTO = cliente.buscaListaProdutos();
+        try {
+            List<ProductsDTO> produtosDTO = cliente.buscaListaProdutos();
 
-        produtosDTO.forEach(produto -> {
-            produtoService.salvaProdutos(produtoConverter.toEntity(produto));
-        });
+            produtosDTO.forEach(produto -> {
+                if (produtoService.existeProdutoPorNome(produto.getNome())) {
+                    return;
+                }
 
-        return produtoConverter.toListDTO(produtoService.buscaTodosProdutos());
+                produtoService.salvaProdutos(produtoConverter.toEntity(produto));
+            });
+
+            return produtoConverter.toListDTO(produtoService.buscaTodosProdutos());
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar e gravar produtos: " + e.getMessage(), e);
+        }
     }
 }
